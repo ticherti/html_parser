e96f111219c379498c3581c532f5a822103cd129
Ticherti <vera.karr@gmail.com>
2021-11-10 01:14:02 +0300

    Add Spring web and thymeleaf. Also include webjars for better style

---
+++ src/main/java/com/github/htmlparser/HtmlparserApplication.java
@@ -0,0 +1,13 @@
+package com.github.htmlparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlparserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlparserApplication.class, args);
+    }
+
+}
